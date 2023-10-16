658e0cce22937c8ab19b940c43d51871e1a2aff9
cloudwithax <heyimnick14@gmail.com>
2023-10-16 14:44:44 -0400

    no more cors in server

--- server.js
+++ server.js
@@ -1,31 +1,15 @@
 const express = require('express');
-const cors = require('cors');
 const path = require('path');
 
 const app = express();
 
-// Enable CORS
-
-
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-    );
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    if (req.method === "OPTIONS") {
-      return res.sendStatus(200);
-    }
-    next();
-});
+
 
 // Serve index.html for all routes
 app.get('*', (req, res,) => {
-    res.header('Access-Control-Allow-Origin', '*');
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
