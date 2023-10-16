feaf0407fbf1c9e9c2cdd7e3e66600a0953c1116
cloudwithax <heyimnick14@gmail.com>
2023-10-16 14:36:09 -0400

    add cors

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 
 const app = express();
+
+// Enable CORS
+app.use(cors());
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
